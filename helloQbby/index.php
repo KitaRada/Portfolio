e3963e4281e00ee76f575bcfbac7906b96a796e3
KitaRada <KitaRada@users.noreply.github.com>
2016-04-12 12:00:47 +0800

    Added files via upload
    I took a webdesign class and this work is as a homework.

---
+++ helloQbby/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML>
+<!--
+	Overflow by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>HelloQbby</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name "author"countent="40041152">
+		<meta name "description" countent="this is just a homework">
+		<meta name "keywords" countent="HelloQbby","來電接送站"> 
+		<link rel = "shortcut icon" href = "images/Qicon.ico" type = "image/x-icon"/>
+		<!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="assets/css/ie/respond.min.js"></script><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<header>
+					<h1>HelloQbby</h1>
+					<p>來電接送站</p>
+				</header>
+				<footer>
+					<a href="#banner" class="button style2 scrolly-middle">了解更多</a>
+				</footer>
+			</section>
+
+		<!-- Banner -->
+			<section id="banner">
+				<header>
+					<h2>系統特色</h2>
+				</header>
+				<p>史上最方便的接送廣播服務，只需開啟來電接送手機<br/>
+				無需安裝任何軟體與採購多餘硬體設備，專線可無限量擴充<br>
+				收費用多少算多少，可接受客製化功能需求開發，完全不用額外收費。<br>
+				完全不需付擔任何通話費用，支援手機與市話<br>
+				服務專線不佔線，平均 0 .1秒可處理１通來電量<br>
+				客製化語音廣播，學員倍感親切
+
+				</p>
+				<footer>
+					<a href="#first" class="button style2 scrolly">繼續閱讀</a>
+				</footer>
+			</section>
+
+		<!-- Feature 1 -->
+			<article id="first" class="container box style1 right">
+				<iframe class="image" width="420" height="315" src="https://www.youtube.com/embed/6tRu7StcupI" frameborder="0" allowfullscreen></iframe>
+				<div class="inner">
+					<header>
+						<h2>HelloQbby展示影片</h2>
+						<p>守護孩子的安全是不能等待的，提昇機構的服務品質是刻不容緩的，即日起～6/30日止，家長來電暨手機搖搖樂廣播功能，開放機構三十天免費體驗試用！</p>
+					</header>
+				</div>
+			</article>
+
+		<!-- Portfolio -->
+			<article class="container box style2">
+				<header>
+					<h2>其他功能</h2>
+				</header>
+				<div class="inner gallery">
+					<div class="row 0%">
+						<div class="3u 12u(mobile)"><a href="images/pic/adult.png" class="image fit"><img src="images/thumbs/01.jpg" alt="" title="家長管理" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/pic/book.png" class="image fit"><img src="images/thumbs/02.jpg" alt="" title="圖書管理" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/pic/call.png" class="image fit"><img src="images/thumbs/03.jpg" alt="" title="來電管理" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/pic/class.png" class="image fit"><img src="images/thumbs/04.jpg" alt="" title="課程管理" /></a></div>
+					</div>
+					<div class="row 0%">
+						<div class="3u 12u(mobile)"><a href="images/pic/money.png" class="image fit"><img src="images/thumbs/05.jpg" alt="" title="收費管理" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/pic/passport.png" class="image fit"><img src="images/thumbs/06.jpg" alt="" title="閱讀護照" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/pic/sing_in.png" class="image fit"><img src="images/thumbs/07.jpg" alt="" title="簽到管理" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/pic/student.png" class="image fit"><img src="images/thumbs/08.jpg" alt="" title="學員管理" /></a></div>
+					</div>
+				</div>
+			</article>
+
+		<!-- Contact -->
+			<article class="container box style3">
+				<header>
+					<h2>聯絡我們</h2>
+					<p>有任何疑問請與我們聯絡</p>
+				</header>
+				<form method="post" action="#">
+					<div class="row 50%">
+						<div class="6u 12u$(mobile)"><input type="text" class="text" name="name" placeholder="機構名稱" /></div>
+						<div class="6u$ 12u$(mobile)"><input type="text" class="text" name="email" placeholder="Email" /></div>
+						<div class="12u$">
+							<textarea name="message" placeholder="請留下信息"></textarea>
+						</div>
+						<div class="12u$">
+							<ul class="actions">
+								<li><input type="submit" value="完成送出" /></li>
+							</ul>
+						</div>
+					</div>
+				</form>
+			</article>
+		<section id="footer">
+			<ul class="icons">
+				<li><a href="https://www.facebook.com/HelloQbby" target="_blank" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+			</ul>
+			<!--<div class="copyright">
+				<ul class="menu">
+					<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+				</ul>
+			</div>-->
+		</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
